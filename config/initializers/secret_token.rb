ad7205e9ec85c7769c84a8c7f38eef1b0690c5bd
wise0neX <sisenda.fred@gmail.com>
2012-10-28 16:23:35 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmashingBoxesDemo::Application.config.secret_token = '00395c2db35f20c1f3e169e582d1e606979644d703fcb253aadc7663c979848d8837663ca1208ba9bc3ed744401713fbf87dfbc211d8e8f51173d8b5a310a0be'
